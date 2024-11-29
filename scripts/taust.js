071e78521e339dc151edfa49061470c6174348ff
taneltooming <taneltooming7@gmail.com>
2024-11-29 13:58:01 +0200

    lisan uuendused

--- scripts/taust.js
+++ scripts/taust.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
-        let scrollPosition = window.scrollY * 0.5;
+        let scrollPosition = window.scrollY * 0.25;
         document.documentElement.style.setProperty('--scroll-offset', `${scrollPosition}px`);
     });
 });
